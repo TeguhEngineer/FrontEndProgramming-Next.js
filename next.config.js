fbeefd8842959e8adb2111414574b9218d37f838
TeguhEngineer <teguhsimdig8@gmail.com>
2025-06-17 20:32:25 +0700

    update next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // isi konfigurasi kamu di sini, contoh:
+    reactStrictMode: true,
+    // basePath: '/some-path',
+};
+
+module.exports = nextConfig;
